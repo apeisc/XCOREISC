d46f78b1f3bca83fcddd1d17c80df83900a9481c
Miguel Angel Sirlopu Cumpa <peru_masc@msn.com>
2013-09-07 11:55:02 -0500

    Revert "translado de repo"
    This reverts commit b335b379bcb70b401fd8cdd95504d2408eb249dc.

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from app.models import *
 from django.contrib import admin
 class PostAdmin(admin.ModelAdmin):
@@ -22,25 +21,3 @@ admin.site.register(Post, PostAdmin)
 admin.site.register(Patrocina, PublicidadAdmin)
 admin.site.register(Pais, PaisAdmin)
 admin.site.register(Alertas,AlertasAdmin)
-=======
-from app.models import *
-from django.contrib import admin
-class PostAdmin(admin.ModelAdmin):
-    list_display = ["asistente","event","time"]
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ["user"]
-class PonenteAdmin(admin.ModelAdmin):
-    list_display = ('ponente_thumb','nombre', 'body')
-class EventsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'body', 'fecha', 'ponente','banner')
-class PublicidadAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'body', 'direccion')
-class PaisAdmin(admin.ModelAdmin):
-    list_display = ["nombre"]
-admin.site.register(Ponente,PonenteAdmin)
-admin.site.register(Events,EventsAdmin)
-admin.site.register(UserProfile, ProfileAdmin)
-admin.site.register(Post, PostAdmin)
-admin.site.register(Patrocina, PublicidadAdmin)
-admin.site.register(Pais, PaisAdmin)
->>>>>>> 8e6e054ba727a25830c8904e1d79682aa9710689
--- app/tests.py
+++ app/tests.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -15,21 +14,3 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
-=======
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
->>>>>>> 8e6e054ba727a25830c8904e1d79682aa9710689
